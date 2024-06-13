b8b44599ab0b667ba27b0b5245308d9890b56f86
Will <48894810+Twillij@users.noreply.github.com>
2024-06-13 17:16:45 +1000

    Added header footer using JS

---
+++ headerfooter.js
@@ -0,0 +1,21 @@
+fetch('header.html')
+    .then(response => {
+        return response.text(); // Convert response into text
+    })
+    .then(data => {
+        document.getElementById('header').innerHTML = data; // Inject header content into the "header" div element.
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    });
+
+fetch('footer.html')
+    .then(response => {
+        return response.text(); // Convert response into text
+    })
+    .then(data => {
+        document.getElementById('footer').innerHTML = data; // Inject footer content into the "footer" div element.
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    });
